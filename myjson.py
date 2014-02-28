8b77974e3dc55b7ab25dc265dafd53ea1417a54d
Jay Prakash Singh <jp213@ymail.com>
2014-02-28 20:13:52 +0530

    Update myjson.py

--- myjson.py
+++ myjson.py
@@ -1,38 +1,50 @@
+#!/usr/bini/env python 
+import json
 import requests
-import ast
+
+url  ="http://www.myntra.com/searchws/search/styleids2"
+
+
+jsonline = {"query":"(global_attr_article_type_facet:(\"Sports Shoes\") AND global_attr_gender:(\"Men\")) AND (count_options_availbale:[1 TO *])","start":24,"rows":50,"sort":[{"sort_field":"count_options_availbale","order_by":"desc"},{"sort_field":"style_store4_sort_field","order_by":"desc"},{"sort_field":"potential_revenue_sort_field","order_by":"desc"},{"sort_field":"global_attr_catalog_add_date","order_by":"desc"}],"return_docs":True,"facetField":["Sport_article_attr","Upper_Material_article_attr","Fastening_article_attr","Outsole_article_attr","Cleats_article_attr","Width_article_attr"],"colour_grouping":True,"fq":[],"facet":False,"mongoData":{"trafficData":{"source":"referral","medium":"google.co.in","campaign":None,"channel":"Organic"},"referer":{"host":"www.myntra.com","path":"/shop/men-footwear","query":"hpbnr=mm-newui-sub-mens-footwear-24sep","domain":"myntra.com"},"deviceData":{"isMobile":False},"mabTestData":{"mabtests":[{"abtestName":"productui","variantName":"newui","filteredOut":"False"},{"abtestName":"telesales","variantName":"shownumber","filteredOut":"False"},{"abtestName":"splash","variantName":"control","filteredOut":"False"},{"abtestName":"loginbox","variantName":"v3","filteredOut":"False"},{"abtestName":"boostedSearch","variantName":"control","filteredOut":"False"},{"abtestName":"newVsOld","variantName":"new","filteredOut":"False","userType":"new"},{"abtestName":"SocialShare","variantName":"control","filteredOut":"False"},{"abtestName":"LoginSplash","variantName":"control","filteredOut":"False"},{"abtestName":"ColourSelect","variantName":"test","filteredOut":"False"},{"abtestName":"NewLayout","variantName":"test","filteredOut":"False"},{"abtestName":"stickySize","variantName":"control","filteredOut":"False"},{"abtestName":"secureSignin","variantName":"secure","filteredOut":"False"},{"abtestName":"newSignupAB","variantName":"control","filteredOut":"False"},{"abtestName":"checkout","variantName":"test","filteredOut":"False"},{"abtestName":"couponAutoApplyTest","variantName":"control","filteredOut":"False"},{"abtestName":"SearchOptim","variantName":"test","filteredOut":"False"},{"abtestName":"loginSignupABTest","variantName":"test","filteredOut":"False"},{"abtestName":"WebEngageFeedback","variantName":"control","filteredOut":"False"},{"abtestName":"WebEngageNotifications","variantName":"test","filteredOut":"False"},{"abtestName":"LocationBreadcrumbABTest","variantName":"test","filteredOut":"False"},{"abtestName":"styleRevenueAdjusted6","variantName":"test4","filteredOut":"False"},{"abtestName":"nudge","variantName":"control","filteredOut":"False"},{"abtestName":"nudgeShow","variantName":"test","filteredOut":"False"},{"abtestName":"regV2","variantName":"test","filteredOut":"False"},{"abtestName":"regV2PhoneRequired","variantName":"control","filteredOut":"False"},{"abtestName":"njswHP","variantName":"control","filteredOut":"False"},{"abtestName":"idptest","variantName":"test","filteredOut":"False"},{"abtestName":"checkout_flow_java","variantName":"java","filteredOut":"False"},{"abtestName":"coupon_applicability","variantName":"cart","filteredOut":"False"},{"abtestName":"promo_coupons","variantName":"validate","filteredOut":"False"},{"abtestName":"quickLookOffer","variantName":"test","filteredOut":"False"},{"abtestName":"promo_coupons_mobile","variantName":"validate","filteredOut":"False"}]}}}
+
+
 
 
 def main3(val):
     for key2, val2 in val.items():
-        print 
-        print  "*"*20, key2, ":",  val2,  "*"*20
+        print "="*20
+        print key2, ":", val2
+        print "*"*20
         main2(key2, val2)
 
 
 def main2(key, val):
+    key = str(key)
+
     if type(val) is dict:
         main3(val)
 
+    elif  type(val) is list:
+        for subval in val:
+            if (type(subval) is dict) or (type(subval) is tuple):
+                main3(subval)
+            else:
+                #print "%s : %s" %(key, subval)
+                pass
     else:
-        key = str(key)
-	val = str(val)
-        print "%s : %s"  %(key, val)
+        print "%s : %s" %(key, val)
+         
+         
+       
 
     
 
 def main():
-    print "*"*20
-    payload ={"name":"jp"}
-    r = requests.post("http://localhost/testphp.php", data=payload)
-    myjson = { 'a':'A', 'b':(2, 4), 'c':3.0, ('d',):{'D tuple': "mytuple", "second key":{"second value":"third value"}}  } 
-    #for key, val in r.json().items():
-    for key, val in myjson.items():
-        #print type(key)
-        #print type(val) 
-        #print str(key)
-	main2(key, val)
+    for key, val in jsonline.items():
+        main2(key, val)
 
 
 
+  
 if __name__=="__main__":
     main()
